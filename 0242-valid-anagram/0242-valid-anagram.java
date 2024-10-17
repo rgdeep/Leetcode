02499fde9c039478d5d14abd80e03f1f826293a1
Deepak Kumar <rgdeep.edu@gmail.com>
2024-10-17 11:53:50 +0530

    Time: 2 ms (97.91%), Space: 43.2 MB (71.49%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,13 @@
+class Solution {
+public boolean isAnagram (String s,String t){
+if(s.length()!=t.length ()) 
+    return false;
+int[]count=new int [128];
+for(char c:s.toCharArray ())
+    ++count[c];
+for(char c:t.toCharArray())
+if(--count[c]<0)
+    return false;
+return true;
+}
+}
