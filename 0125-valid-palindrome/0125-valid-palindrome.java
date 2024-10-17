65b2400a2ad5032781ed05fe062e2fe3dce1c2eb
Deepak Kumar <rgdeep.edu@gmail.com>
2024-10-17 11:53:11 +0530

    Time: 16 ms (19.58%), Space: 44.9 MB (30.63%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,17 @@
+public class Solution {
+    static boolean isPalindrome(String s) {
+        s = s.replaceAll("[^A-Za-z0-9]", "");
+        s = s.toLowerCase();
+        int i = 0;
+        int j = s.length() - 1;
+        while (i <= j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+}
